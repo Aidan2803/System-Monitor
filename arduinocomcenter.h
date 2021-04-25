350eb83d0a80d698e87a992377617d929c415705
Aidan <aid2803@ukr.net>
2021-04-25 21:53:00 +0300

    updated arduinocom.h

--- arduinocomcenter.h
+++ arduinocomcenter.h
@@ -2,6 +2,7 @@
 #define ARDUINOCOMCENTER_H
 
 #include <globalVariables.h>
+#include <windows.h>
 
 class ArduinoComCenter
 {
@@ -11,7 +12,30 @@ public:
     void getInfoForPackage();
 
 private:
+
+    enum PackageInfo{
+        CPU_LOAD,
+        CPU_TEMP,
+        GPU_TEMP,
+        RAM_LOAD,
+        HDD1_TEMP,
+        HDD2_TEMP,
+        HDD3_TEMP,
+        HDD4_TEMP
+    };
+
+    enum UpTimeInfo{
+        HOURS,
+        MINUTES,
+        SECONDS,
+        MAX_AMOUNT
+    };
+
+
     char packageForTranser[PACKAGE_SIZE];
+
+    LPCTSTR m_comName;
+
     int CPU_Load;
     int CPU_Temp;
     int GPU_Temp;
@@ -20,7 +44,9 @@ private:
     int HDD2_Temp;
     int HDD3_Temp;
     int HDD4_Temp;
-    //UPTIME
+    int UpTimeArray[MAX_AMOUNT];
+
+
 
     void openComPort();
     void writeToComPort();
